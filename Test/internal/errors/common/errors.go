c81a109884599e50dd335cea8e130a3d7dc8ec5f
Бароян Гиоргий <giorgiy_03@mail.ru>
2025-06-15 21:49:15 +0300

    ref

--- Test/internal/errors/common/errors.go
+++ Test/internal/errors/common/errors.go
@@ -1,17 +1,15 @@
 package common
 
-import ()
-
 type CommonError struct {
 	Code    int
 	Message string
 }
 
 var (
-	BadRequest          = CommonError{Code: 1, Message: "errors.common.BadRequest"}
-	Unauthorized        = CommonError{Code: 2, Message: "errors.common.Unauthorized"}
-	Forbidden           = CommonError{Code: 3, Message: "errors.common.Forbidden"}
+	BadRequest          = CommonError{Code: 1, Message: "errors.common.badRequest"}
+	Unauthorized        = CommonError{Code: 2, Message: "errors.common.unauthorized"}
+	Forbidden           = CommonError{Code: 3, Message: "errors.common.forbidden"}
 	NotFoundError       = CommonError{Code: 4, Message: "errors.common.notFound"}
-	InternalServerError = CommonError{Code: 5, Message: "errors.common.InternalServerError"}
-	ServiceUnavailable  = CommonError{Code: 6, Message: "errors.common.ServiceUnavailable"}
+	InternalServerError = CommonError{Code: 5, Message: "errors.common.internalServerError"}
+	ServiceUnavailable  = CommonError{Code: 6, Message: "errors.common.serviceUnavailable"}
 )
