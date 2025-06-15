2f49bac972c4894f47051586bc0e1e26b438acdd
Бароян Гиоргий <giorgiy_03@mail.ru>
2025-06-15 21:38:06 +0300

    ref

--- Test/internal/http/httpresponse/response/response.go
+++ Test/internal/http/httpresponse/response/response.go
@@ -3,27 +3,29 @@ package response
 import "myapp/internal/errors/common"
 
 type (
-	Success struct {
+	ResponseData struct {
 		Data any `json:"data"`
 	}
 
-	Err struct {
+	Error struct {
 		Message string                 `json:"message"`
 		Details map[string]interface{} `json:"details"`
 		Code    int                    `json:"code"`
 	}
 )
 
-func NewSuccess(data any) Success {
-	return Success{
+func NewSuccess(data any) ResponseData {
+	return ResponseData{
 		Data: data,
 	}
 }
 
-func NewError(err common.CommonError, details map[string]interface{}) Err {
-	return Err{
-		Code:    err.Code,
-		Message: err.Message,
-		Details: details,
+func NewError(err common.CommonError, details map[string]interface{}) ResponseData {
+	return ResponseData{
+		Data: Error{
+			Code:    err.Code,
+			Message: err.Message,
+			Details: details,
+		},
 	}
 }
