81150d93ad4f36e5e3ecd11211a0aa3c0b387b23
Бароян Гиоргий <giorgiy_03@mail.ru>
2025-06-16 20:25:29 +0300

    ref

--- Test/internal/models/goods.go
+++ Test/internal/models/goods.go
@@ -1,19 +1,18 @@
 package models
 
 import (
-	"github.com/google/uuid"
 	"github.com/uptrace/bun"
+	"time"
 )
 
 type Goods struct {
 	bun.BaseModel `bun:"table:goods"`
 
-	Id         uuid.UUID `bun:"uuid"`
-	FirstName  string    `bun:"first_name"`
-	LastName   string    `bun:"last_name"`
-	Patronymic string    `bun:"patronymic"`
-	City       string    `bun:"city"`
-	Phone      string    `bun:"phone"`
-	Email      string    `bun:"email"`
-	Password   string    `bun:"password"`
+	Id          int       `json:"id" bun:"id"`
+	ProjectID   int       `json:"project_id" bun:"project_id"`
+	Name        string    `json:"name" bun:"name"`
+	Description string    `json:"description" bun:"description"`
+	Priority    int       `json:"priority" bun:"priority"`
+	Removed     bool      `json:"removed" bun:"removed"`
+	CreatedAt   time.Time `json:"created_at" bun:"created_at"`
 }
