4b5d6f1d06a31122e9663c9e18814adf0ed62d6d
Бароян Гиоргий <giorgiy_03@mail.ru>
2025-06-16 22:04:23 +0300

    ref

--- Test/internal/repository/goods_redis.go
+++ Test/internal/repository/goods_redis.go
@@ -3,25 +3,23 @@ package repository
 import (
 	"context"
 	"fmt"
-	redis2 "github.com/redis/go-redis/v9"
-	"myapp/pkg/redis"
+	"github.com/redis/go-redis/v9"
+	connector "myapp/pkg/redis"
 	"time"
 )
 
-func NewGoodsRedis(rdb *redis.Redis) *GoodsRedis {
-	return &GoodsRedis{
-		Rdb: rdb,
-	}
+func NewGoodsRedis(rdb *connector.Redis) *GoodsRedis {
+	return &GoodsRedis{rdb}
 }
 
 type GoodsRedis struct {
-	Rdb *redis.Redis
+	*connector.Redis
 }
 
 func (db *GoodsRedis) GetRoleRights(ctx context.Context, role string) ([]string, error) {
-	rights, err := db.Rdb.Conn.SMembers(ctx, role).Result()
+	rights, err := db.Conn.SMembers(ctx, role).Result()
 	if err != nil {
-		if err == redis2.Nil {
+		if err == redis.Nil {
 			return nil, nil
 		}
 		return nil, fmt.Errorf("AuthRedis - GetRoleRights - db.Rdb.SMembers: %w", err)
@@ -31,11 +29,11 @@ func (db *GoodsRedis) GetRoleRights(ctx context.Context, role string) ([]string,
 }
 
 func (db *GoodsRedis) AddRoleRights(ctx context.Context, role string, rights []string, period time.Duration) error {
-	err := db.Rdb.Conn.SAdd(ctx, role, rights).Err()
+	err := db.Conn.SAdd(ctx, role, rights).Err()
 	if err != nil {
 		return fmt.Errorf("AuthRedis - AddRoleRights - db.Rdb.SAdd: %w", err)
 	}
-	err = db.Rdb.Conn.Expire(ctx, role, period).Err()
+	err = db.Conn.Expire(ctx, role, period).Err()
 	if err != nil {
 		return fmt.Errorf("AuthRedis - AddRoleRights - db.Rdb.Expire: %w", err)
 	}
