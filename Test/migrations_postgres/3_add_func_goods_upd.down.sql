ada6c52db88b4b876de424ef7aec349e39b65236
Бароян Гиоргий <giorgiy_03@mail.ru>
2025-06-23 22:34:45 +0300

    Revert "ref"
    This reverts commit 9077e6d008a083f20159234c94b141dd98003f95.

---
+++ Test/migrations_postgres/3_add_func_goods_upd.down.sql
@@ -0,0 +1,1 @@
+DROP FUNCTION IF EXISTS goods_upd(_src JSONB);
---
+++ Test/migrations_postgres/4_add_func_goods_list.down.sql
@@ -0,0 +1,4 @@
+DROP FUNCTION IF EXISTS goods_list(_goods_id INTEGER,
+                                   _project_id INTEGER,
+                                   _limit INTEGER,
+                                   _offset INTEGER);
