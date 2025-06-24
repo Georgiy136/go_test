49b09cf2b66b07b3b1815f61d58555fd822a336b
Бароян Гиоргий <giorgiy_03@mail.ru>
2025-06-24 19:43:36 +0300

    change dir

--- Go_Test/internal/http/router.go
+++ Go_Test/internal/http/router.go
@@ -2,10 +2,7 @@ package http
 
 import (
 	"github.com/gin-gonic/gin"
-	swaggerFiles "github.com/swaggo/files"
-	ginSwagger "github.com/swaggo/gin-swagger"
 
-	_ "myapp/docs"
 	"myapp/internal/usecase"
 )
 
@@ -13,9 +10,6 @@ func NewRouter(router *gin.Engine, os usecase.GoodsUseCases) {
 	goodsHandlers := &GoodsHandler{
 		us: os,
 	}
-
-	// Swagger
-	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	// Routers
 	api := router.Group("/api")
