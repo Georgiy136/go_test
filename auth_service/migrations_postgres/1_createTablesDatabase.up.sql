e6a0e8e55f7ff08e3339f90f449d330c2d3c42a7
Бароян Гиоргий <giorgiy_03@mail.ru>
2025-07-12 22:10:07 +0300

    ref

--- auth_service/migrations_postgres/1_createTablesDatabase.up.sql
+++ auth_service/migrations_postgres/1_createTablesDatabase.up.sql
@@ -1,19 +1,12 @@
-CREATE SCHEMA IF NOT EXISTS login;
+CREATE SCHEMA IF NOT EXISTS sessions;
 
-CREATE TABLE IF NOT EXISTS login.user_login (
+CREATE TABLE IF NOT EXISTS sessions.user_login (
     user_id integer NOT NULL,
-    refresh_token_id character varying NOT NULL,
+    session_id character varying NOT NULL,
     user_agent character varying NOT NULL,
     ip_address character varying NOT NULL,
-    CONSTRAINT pk_user_login PRIMARY KEY (user_id, user_agent, ip_address)
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    CONSTRAINT pk_user_login PRIMARY KEY (user_id, session_id)
 );
 
-CREATE TABLE IF NOT EXISTS login.refresh_tokens (
-    refresh_token_id SERIAL PRIMARY KEY,
-    refresh_token TEXT,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
-);
-
-CREATE INDEX IF NOT EXISTS idx_user_login_user_id on login.user_login (user_id);
-
-CREATE INDEX IF NOT EXISTS idx_refresh_tokens_refresh_token_id on login.refresh_tokens (refresh_token_id);
+CREATE INDEX IF NOT EXISTS idx_user_login_user_id on sessions.user_login (user_id);
