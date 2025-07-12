43e0f161a658afb86f5df056855dd04caa4153c6
Бароян Гиоргий <giorgiy_03@mail.ru>
2025-07-12 22:14:06 +0300

    ref

--- auth_service/migrations_postgres/1_createTablesDatabase.up.sql
+++ auth_service/migrations_postgres/1_createTablesDatabase.up.sql
@@ -3,6 +3,7 @@ CREATE SCHEMA IF NOT EXISTS sessions;
 CREATE TABLE IF NOT EXISTS sessions.user_login (
     user_id integer NOT NULL,
     session_id character varying NOT NULL,
+    hash_refresh_token character varying NOT NULL,
     user_agent character varying NOT NULL,
     ip_address character varying NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
