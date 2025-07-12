13e43ae9eb04d85ce74358b9b02ff54eeaeaf874
Бароян Гиоргий <giorgiy_03@mail.ru>
2025-07-12 22:18:57 +0300

    ref

--- auth_service/client/user_info_client.go
+++ auth_service/client/user_info_client.go
@@ -19,32 +19,8 @@ func NewUserInfoClient(cfg config.UserInfoClient) *UserInfoClient {
 	}
 }
 
+// mock функция
 func (n *UserInfoClient) GetUserInfo(ctx context.Context, userID int) (*models.User, error) {
-	/*request, err := http.NewRequest(http.MethodGet, n.cfg.Url+"?"+strconv.Itoa(userID), nil)
-	if err != nil {
-		return nil, fmt.Errorf(" http.NewRequest req error: %v", err)
-	}
-
-	request.Header.Set("Content-Type", "application/json")
-
-	resp, err := n.httpClient.Do(request)
-	if err != nil {
-		return nil, fmt.Errorf(" n.httpClient.Do req error: %v", err)
-	}
-
-	if resp.StatusCode == http.StatusOK {
-		var user *models.User
-		response, err := io.ReadAll(resp.Body)
-		if err != nil {
-			return nil, fmt.Errorf(" io.ReadAll req error: %v", err)
-		}
-		if err = jsoniter.Unmarshal(response, user); err != nil {
-			return nil, fmt.Errorf(" jsoniter.Unmarshal req error: %v", err)
-		}
-		return user, nil
-	}
-
-	return nil, fmt.Errorf("http status code %d", resp.StatusCode)*/
 
 	return &models.User{UserID: userID}, nil
 }
