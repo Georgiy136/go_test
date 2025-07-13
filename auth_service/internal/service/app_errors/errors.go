eb82ca6ece8c14f3a9a3590491a260cdae80388a
Бароян Гиоргий <giorgiy_03@mail.ru>
2025-07-13 16:56:36 +0300

    ref

--- auth_service/internal/service/app_errors/errors.go
+++ auth_service/internal/service/app_errors/errors.go
@@ -5,6 +5,5 @@ import "errors"
 var (
 	TokenIsExpiredError      = errors.New("token is expired")
 	SessionUserNotFoundError = errors.New("user session not found")
-	UserNotFoundError        = errors.New("user not found")
 	UserAgentNotMatchInDB    = errors.New("User-Agent does not match in db")
 )
