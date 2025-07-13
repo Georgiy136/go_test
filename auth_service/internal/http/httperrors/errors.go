77b14464dab0a50d62165ec1ef497412d2109bf9
Бароян Гиоргий <giorgiy_03@mail.ru>
2025-07-13 17:18:58 +0300

    ref

--- auth_service/internal/http/httperrors/errors.go
+++ auth_service/internal/http/httperrors/errors.go
@@ -4,24 +4,24 @@ import (
 	"net/http"
 )
 
-type CommonError struct {
+type CommonHttpError struct {
 	Code     int
 	Message  string
 	HttpCode int
 }
 
 var (
-	BadRequest                 = CommonError{Code: 1, Message: "errors.templates.badRequest", HttpCode: http.StatusBadRequest}
-	Unauthorized               = CommonError{Code: 2, Message: "errors.templates.unauthorized", HttpCode: http.StatusUnauthorized}
-	Forbidden                  = CommonError{Code: 3, Message: "errors.templates.forbidden", HttpCode: http.StatusForbidden}
-	NotFoundError              = CommonError{Code: 4, Message: "errors.templates.notFound", HttpCode: http.StatusNotFound}
-	InternalServerError        = CommonError{Code: 5, Message: "errors.templates.internalServerError", HttpCode: http.StatusInternalServerError}
-	ServiceUnavailable         = CommonError{Code: 6, Message: "errors.templates.serviceUnavailable", HttpCode: http.StatusServiceUnavailable}
-	ServiceUnprocessableEntity = CommonError{Code: 7, Message: "errors.templates.unprocessableEntity", HttpCode: http.StatusUnprocessableEntity}
+	BadRequest                 = CommonHttpError{Code: 1, Message: "errors.templates.badRequest", HttpCode: http.StatusBadRequest}
+	Unauthorized               = CommonHttpError{Code: 2, Message: "errors.templates.unauthorized", HttpCode: http.StatusUnauthorized}
+	Forbidden                  = CommonHttpError{Code: 3, Message: "errors.templates.forbidden", HttpCode: http.StatusForbidden}
+	NotFoundError              = CommonHttpError{Code: 4, Message: "errors.templates.notFound", HttpCode: http.StatusNotFound}
+	InternalServerError        = CommonHttpError{Code: 5, Message: "errors.templates.internalServerError", HttpCode: http.StatusInternalServerError}
+	ServiceUnavailable         = CommonHttpError{Code: 6, Message: "errors.templates.serviceUnavailable", HttpCode: http.StatusServiceUnavailable}
+	ServiceUnprocessableEntity = CommonHttpError{Code: 7, Message: "errors.templates.unprocessableEntity", HttpCode: http.StatusUnprocessableEntity}
 )
 
 type CustomError struct {
-	Err         *CommonError
+	Err         *CommonHttpError
 	Description string
 }
 
